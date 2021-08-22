c56354275acedbaaf4df0a195de48c99d6e9cd92
“Huang <10541246@cycu.org.tw>
2021-08-22 21:46:34 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,22 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  freeMode: true,
+  loop: true,
+  grabCursor: true,
+  breakpoints:{
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
+});
